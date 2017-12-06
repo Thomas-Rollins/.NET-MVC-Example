587b3104840b79eefdc7c7360b04650b199e3a65
Thomas Rollins <Thomas.Rollins@hotmail.ca>
2017-12-06 12:20:03 -0500

    Add project files.

---
+++ COMP2007_Assignment_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2007_Assignment_2.MvcApplication" Language="C#" %>
---
+++ COMP2007_Assignment_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_Assignment_2.Startup))]
+namespace COMP2007_Assignment_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
